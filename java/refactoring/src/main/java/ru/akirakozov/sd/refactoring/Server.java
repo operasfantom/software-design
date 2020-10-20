89ef61f02991b7b63b398c427a2cd53e3fc63efc
Pavel Yatcheniy <yatcheniy.pavel@huawei.com>
2020-10-20 16:54:35 +0300

    extract Server class

--- java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/Server.java
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/Server.java
@@ -1,6 +1,5 @@
 package ru.akirakozov.sd.refactoring;
 
-import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import ru.akirakozov.sd.refactoring.servlet.AddProductServlet;
@@ -9,13 +8,16 @@ import ru.akirakozov.sd.refactoring.servlet.QueryServlet;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
  * @author akirakozov
  */
-public class Main {
-    public static void main(String[] args) throws Exception {
+public class Server {
+    private final org.eclipse.jetty.server.Server server;
+
+    public Server(int port) throws SQLException {
         try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
             String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
                     "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
@@ -27,17 +29,31 @@ public class Main {
             stmt.close();
         }
 
-        Server server = new Server(8081);
+        server = new org.eclipse.jetty.server.Server(port);
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
 
         context.addServlet(new ServletHolder(new AddProductServlet()), "/add-product");
-        context.addServlet(new ServletHolder(new GetProductsServlet()),"/get-products");
-        context.addServlet(new ServletHolder(new QueryServlet()),"/query");
+        context.addServlet(new ServletHolder(new GetProductsServlet()), "/get-products");
+        context.addServlet(new ServletHolder(new QueryServlet()), "/query");
+    }
 
+    public static void main(String[] args) throws Exception {
+        assert args != null : "args are null";
+        assert args.length == 1 : "args length is wrong:" + args.length;
+        int port = Integer.parseInt(args[0]);
+        Server server = new Server(port);
         server.start();
+        server.join();
+    }
+
+    public void start() throws Exception {
+        server.start();
+    }
+
+    public void join() throws InterruptedException {
         server.join();
     }
 }
